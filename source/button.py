ed45393da8b1554090b026ecc57566165628cde1
Trần Văn Truyền <19522448@gm.uit.edu.vn>
2023-06-16 00:38:16 +0700

    docstring menu

--- source/button.py
+++ source/button.py
@@ -1,4 +1,7 @@
 class Button():
+	"""
+	Vẽ bottom và kiểm tra va cham của bottom 
+	"""
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
 		self.image = image
 		self.x_pos = pos[0]
